06048ee98810cf4c1da29f859bb5e1b397ee237a
人间拾月 <3201773187@qq.com>
2026-05-27 20:08:41 +0800

    fix: 更新代理目标地址，从 localhost:8080 更改为 localhost:8000，md文档更新

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
